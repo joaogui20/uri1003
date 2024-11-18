e6673e563bbbed1ee299cf4d5132db49cfbfd1fb
Fey <Fey@DESKTOP-M63LBFO>
2024-11-18 15:56:00 -0400

    Adicionar arquivos de projeto.

---
+++ uri1003/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace uri1003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // URI 1003 - Faça um programa para ler dois valores inteiros,
+            // e depois mostrar na tela a soma desses números com uma mensagem
+            // explicativa, conforme exemplos.
+            
+
+        }
+    }
+}
